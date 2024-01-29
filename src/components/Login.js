15c2eee0c93f3f3915bcebd8f40b5bc69fb2c9ae
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-01-29 13:43:13 +0300

    add function login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import {useState} from 'react';
 import './login.css';
 import { FaUser,FaLock  } from "react-icons/fa";
 
 export function LoginForm() {
+
+    const [nameUser,setEmail]=useState("");
+    const [passUser,setPass] = useState("");
+    
+    function saveId(userId) {
+        localStorage.setItem('id', userId);
+    }
+    async function checkPersonne() {
+        let item = { nameUser, passUser };
+        try {
+            let response = await fetch("https://backend-production-b756.up.railway.app/personne/checkPersonne", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(item),
+            });
+            if (response.ok) {
+                let responseBody = await response.json();
+                if (responseBody === 0) {
+                    console.warn('Invalid credentials');
+                    alert('Invalid credentials. Please try again.');
+                } else {
+                    console.log('User ID:', responseBody);
+                    saveId(responseBody);
+                }
+            } else {
+                console.warn('Server responded with an error:', response.status);
+            }
+        } catch (error) {
+            console.error('An error occurred during the fetch:', error);
+        }
+    }
   return (
     <div className="back">
     <div className="wrapper">
@@ -11,9 +45,8 @@ export function LoginForm() {
             <div className="input-box">       
                 <input
                     type="email"
-                    placeholder="Email"
-                    // value={email}
-                    // onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Nom de l'utilisateur"
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                 />
                 <FaUser className="icon"/>
@@ -22,8 +55,7 @@ export function LoginForm() {
                 <input
                     type="password"
                     placeholder="Mot de passe"
-                    // value={mdp}
-                    // onChange={(e) => setMdp(e.target.value)}
+                    onChange={(e) => setPass(e.target.value)}
                     required
                 />
                 <FaLock className="icon"/>
@@ -32,7 +64,7 @@ export function LoginForm() {
                 <label><input type="checkbox" />Remember me</label>
                 <a href="#">Forgot password</a>
             </div>
-            <button type="submit">Se connecter</button>
+            <button type="submit" onClick={checkPersonne}>Se connecter</button>
             <div className="register-link">
                 <p>Don't have an account? <a href="#">Register</a></p>
             </div>
